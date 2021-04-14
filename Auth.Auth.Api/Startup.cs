5f0eedeb0e286045e51f4d46af10e6e0c2943b15
Rami Hikmat <rami@ramihikmat.net>
2021-04-14 13:23:22 +1000

    Removed unnecessary package

--- Auth.Auth.Api/Startup.cs
+++ Auth.Auth.Api/Startup.cs
@@ -28,8 +28,7 @@ namespace Auth.Auth.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers()
-                .AddHybridModelBinder();
+            services.AddControllers();
             services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo {Title = "Auth.Auth.Api", Version = "v1"}); });
 
             services.AddSingleton<UserFactory>();
